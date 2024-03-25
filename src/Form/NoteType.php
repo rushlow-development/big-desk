444ac557c776e4544da02dff2e269afb46f81687
Jesse Rushlow <jr@rushlow.dev>
2024-03-25 11:49:07 -0400

    fix note textarea input
    - added missing `textarea` for note "content"
    - misc aesthetic fixes for form buttons / layout

--- src/Form/NoteType.php
+++ src/Form/NoteType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Form\Dto\NoteObject;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +15,8 @@ class NoteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
+            ->add('title', TextType::class)
+            ->add('content', TextareaType::class)
         ;
     }
 
